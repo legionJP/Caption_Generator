1dd6db848a515216b777634b31453a7eccd67d4b
= <=>
2024-09-09 18:42:41 +0530

    update

--- website/uploader/views.py
+++ website/uploader/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello here you ar for the video Uploader")
+    return HttpResponse("Hello  upload your video here for the captions ")
